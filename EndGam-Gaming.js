2e81270861e6a918446ad3a6025db0b259ba18d6
danangsptro <danangsaputro300@gmail.com>
2020-08-06 10:44:54 +0700

    endgame

---
+++ EndGam-Gaming.js
@@ -0,0 +1,64 @@
+// NAVBAR
+$(document).ready(function() {
+    $('.nbn').on({
+        mouseenter: function() {
+            $(this).css('color', 'purple')
+        },
+        mouseleave: function() {
+            $(this).css('color', 'white')
+        }
+    })
+})
+$(document).ready(function() {
+    $('#gn').on({
+        mouseenter: function() {
+            $(this).css('color', 'purple')
+        },
+        mouseleave: function() {
+            $(this).css('color', 'white')
+        }
+    })
+})
+$(document).ready(function() {
+    $('#ter').on({
+        mouseenter: function() {
+            $(this).css('color', 'purple')
+        },
+        mouseleave: function() {
+            $(this).css('color', 'white')
+        }
+    })
+})
+
+// BUTTON CONTENT2 
+$(document).ready(function() {
+    $('.hvr').on({
+        mouseenter: function() {
+            $(this).css('background-color', ' rgb(56, 8, 56)')
+        },
+        mouseleave: function() {
+            $(this).css('background-color', 'purple')
+        }
+    })
+})
+
+// BUTTON CONTENT6
+$(document).ready(function() {
+        $('.contn6').on({
+            mouseenter: function() {
+                $(this).css('color', 'rgb(165, 59, 165)')
+            },
+            mouseleave: function() {
+                $(this).css('color', 'white')
+            }
+        })
+    })
+    // $(document).ready(function(){
+    //     $('.logoa').on({
+    //         mouseenter : function (){
+    //             $(this).css('background-color', 'purple')
+    //         },
+    //         mouseleave : function(){
+    //             $(this).css('background-color', 'white')
+    //         }
+    //     })
